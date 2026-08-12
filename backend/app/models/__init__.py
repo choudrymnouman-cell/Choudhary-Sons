8e7b8725c62bac34d5cdbb2f662be0fc102517cf
Nomi Developer <choudrymnouman@gmail.com>
2026-08-12 08:04:25 -0700

    Initialize backend models package

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.core import Attendance, Client, Employee, Project, ProjectStatus, User, UserRole
+
+__all__ = [
+    "User",
+    "UserRole",
+    "Employee",
+    "Client",
+    "Project",
+    "ProjectStatus",
+    "Attendance",
+]
